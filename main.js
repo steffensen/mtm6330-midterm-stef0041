2a2cb07bcb12f2aad956152e26fa10948c3d732f
Tyler Steffensen <stef0041@algonquinlive.com>
2018-10-17 23:32:24 -0400

    the clean up

---
+++ main.js
@@ -0,0 +1,17 @@
+var modal = document.getElementById('hideMe')
+var btn = document.getElementById('head')
+var span = document.getElementsByClassName('close')[0]
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = 'block'
+}
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = 'none'
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none'
+  }
+}
